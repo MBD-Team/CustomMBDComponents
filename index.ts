a03f746a884d0fc8e4ece7bebca86c5fdc2a9a7f
MBDBot <sportbutler.team@gmail.com>
2022-07-19 12:56:39 +0000

    export types from calendar

--- index.ts
+++ index.ts
@@ -2,3 +2,5 @@ export { default as Modal } from './src/components/Modal.vue';
 export { default as Spinner } from './src/components/Spinner.vue';
 export { default as LoadingButton } from './src/components/LoadingButton.vue';
 export { default as Calendar } from './src/components/Calendar/Calendar.vue';
+export { Group as CalendarGroup } from './src/components/Calendar/Calendar.vue';
+export { Event as CalendarEvent } from './src/components/Calendar/Calendar.vue';
