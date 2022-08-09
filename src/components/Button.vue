a250c8687e1ad79552d5ed8d7d657ab93f3104c1
MBDBot <sportbutler.team@gmail.com>
2022-08-09 11:00:01 +0000

    merge button and loadingbutton

---
+++ src/components/Button.vue
@@ -0,0 +1,60 @@
+<template>
+  <button v-bind="$attrs" :disabled="loading">
+    <Spinner v-if="loading" :color="spinnerColor" class="me-2" :size="spinnerSize"></Spinner>
+    <slot></slot>
+  </button>
+</template>
+<script lang="ts">
+export default {
+  inheritAttrs: false,
+};
+</script>
+<script setup lang="ts">
+import { toRefs } from 'vue';
+import Spinner from './Spinner.vue';
+
+const props = withDefaults(
+  defineProps<{
+    spinnerColor?: 'primary' | 'secondary' | 'light' | 'dark' | 'danger' | 'warning' | 'info' | 'success' | '';
+    spinnerSize?: string;
+    loading?: boolean;
+  }>(),
+  {
+    spinnerColor: '',
+    spinnerSize: '1em',
+    loading: false,
+  }
+);
+const { spinnerColor, spinnerSize } = toRefs(props);
+</script>
+<style scoped lang="scss">
+button {
+  height: 2.5rem;
+  width: 100%;
+  color: black;
+  padding-inline: 0.8rem;
+  font-size: 1.3rem;
+  line-height: 1rem;
+  border-color: black;
+  border-width: 1px;
+  border-radius: 0.5rem;
+  transition: all 0.25s ease-out;
+  &:hover {
+    @media (pointer: fine) {
+      filter: brightness(95%);
+    }
+  }
+  &:active {
+    @media (pointer: fine) {
+      filter: brightness(85%);
+    }
+    @media (pointer: coarse) {
+      filter: brightness(85%);
+    }
+  }
+  &:disabled,
+  &[disabled] {
+    filter: grayscale(50%);
+  }
+}
+</style>
