c56887281933084ec539261ff0b8122e15a55d99
MBDBot <sportbutler.team@gmail.com>
2022-08-16 14:35:27 +0000

    alert with timer

---
+++ src/components/Alert.vue
@@ -0,0 +1,56 @@
+<template>
+  <div v-if="show.show" @click="dismiss()" class="alert" :class="alertClass" role="alert" style="position: relative">
+    <button
+      v-if="closeBtn"
+      style="position: absolute; top: 16px; right: 16px"
+      type="button"
+      class="btn-close shadow-none"
+      aria-label="Close"
+    ></button>
+    <slot></slot>
+  </div>
+</template>
+<script setup lang="ts">
+import { toRefs, watchEffect } from 'vue';
+
+const props = withDefaults(
+  defineProps<{
+    alertClass?: string;
+    show: { show: boolean };
+    dissmissable?: boolean;
+    timer?: number;
+    closeBtn?: boolean;
+  }>(),
+  {
+    alertClass: 'alert-primary',
+    timer: 5000,
+    dissmissable: true,
+    closeBtn: true,
+  }
+);
+const { alertClass, show, timer, dissmissable, closeBtn } = toRefs(props);
+
+let timeout: any;
+
+watchEffect(() => {
+  if (show.value.show) {
+    if (timer.value == 0) return;
+    timeout = setTimeout(() => {
+      show.value.show = false;
+      dismiss();
+    }, timer.value);
+  }
+});
+
+function dismiss() {
+  if (dissmissable.value) {
+    show.value.show = false;
+    clearTimeout(timeout);
+  }
+}
+</script>
+<style lang="scss" scoped>
+.alert {
+  border-radius: 0.5rem;
+}
+</style>
---
+++ src/stories/other Components/Alert.stories.ts
@@ -0,0 +1,38 @@
+import Alert from '../../components/Alert.vue';
+import Button from '../../components/Button.vue';
+
+import { Meta, StoryFn } from '@storybook/vue3';
+import { ref } from 'vue';
+
+export default {
+  title: 'Alert',
+  component: Alert,
+  argTypes: {},
+};
+//👇 We create a “template” of how args map to rendering
+const AlertFromTemplate =
+  (template: (args: string) => string): StoryFn<typeof Alert> =>
+  (args: any) => {
+    const otherArgs = Object.entries(args)
+      .map(([key, value]) => `:${key}='${JSON.stringify(value)}'`)
+      .join(' ');
+    return {
+      components: { Alert, Button },
+      setup() {
+        return { args, console, props: ref({ show: true }) };
+      },
+      template: template(otherArgs),
+    };
+  };
+
+//👇 Each story then reuses that template
+export const BasicAlert = AlertFromTemplate(
+  args =>
+    String.raw`
+    <div class="w-25 m-5"><Button type="button" @click="props.show = true">spawn alert</Button></div>
+    <Alert :show="props"  alertClass="alert-primary w-100" ${args}>
+        <div>testdyfbdfbdgfbdfbebresövnfjaiwbvaewbvibvbi</div>
+        <div>testdsvihvjibdfsvbsjibvdjisbvdsb vdfsbvkjsb vkbdvskjvbsjibvkisdb vo</div>
+    </Alert>
+  `
+);
