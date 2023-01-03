8594a4f74739afeff85bfe016b6c91b48d459c86
dsalex1 <dsalex1@ymail.com>
2023-01-03 14:44:52 +0000

    added proper disabled to multiselect

--- src/components/SexyInputs/MultiSelect.vue
+++ src/components/SexyInputs/MultiSelect.vue
@@ -27,6 +27,7 @@
         @focus="onFocus"
         @blur="onBlur"
         autocomplete="off"
+        :disabled="disabled"
       />
       <!-- label for select -->
       <label class="text" :class="labelClass">
@@ -79,7 +80,7 @@
       :class="multiSelectClass(multi)"
     >
       {{ optionProjection(options.find(e => keyExtractor(e) == keyExtractor(multi))) }}
-      <span @click="deleteItem(multi, index)" style="cursor: pointer">
+      <span @click="deleteItem(multi, index)" style="cursor: pointer" v-if="!disabled">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
           <path
             d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"
@@ -116,6 +117,7 @@ const emit = defineEmits([
 ]);
 const props = withDefaults(
   defineProps<{
+    disabled?: boolean;
     modelValue: string;
     options: any[];
     selected: any[];
@@ -166,6 +168,7 @@ const props = withDefaults(
   }
 );
 const {
+  disabled,
   modelValue,
   noElementMessage,
   listClass,
