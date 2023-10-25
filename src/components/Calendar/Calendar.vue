639c4398fc177fb58cb3496feaf9f42bb56e7f98
test <test@test.com>
2023-10-25 08:14:42 +0000

    fix

--- src/components/Calendar/Calendar.vue
+++ src/components/Calendar/Calendar.vue
@@ -87,7 +87,7 @@
               <div class="h2 d-none d-sm-block fw-normal p-2 rounded-circle" :class="getDayClasses(date, 'num')">
                 <div class="d-flex justify-content-center">{{ date.day }}</div>
               </div>
-              <div><slot name="subHeaderSlot"></slot></div>
+              <slot name="subHeaderSlot"></slot>
             </div>
           </div>
           <!-- date content -->
